cd6b1cdb9a979811edf3728c0678fe39165c5e5d
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-12-22 00:23:03 +0200

    task 2 finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const markup = createGalleryItem(galleryItems);
+let instance = null;
+
+galleryEl.innerHTML = markup;
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onClickOpenPicture(event) {
+  const { target } = event;
+  event.preventDefault();
+
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const chosenPicture = target.dataset.source;
+
+  instance = basicLightbox.create(`
+    <img src="${chosenPicture}" width="800" height="600">
+  `);
+
+  instance.show();
+}
+
+galleryEl.addEventListener("click", onClickOpenPicture);
+
+function onEscCloseModal(event) {
+  if (event.key === "Escape" && instance.visible()) {
+    instance.close();
+  }
+}
+
+document.addEventListener("keydown", onEscCloseModal);
